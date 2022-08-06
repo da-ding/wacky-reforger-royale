bf6379fdec07bd6bf3b0dd8eed53a3599f73451f
Da-ding! <109660283+da-ding@users.noreply.github.com>
2022-08-06 15:11:55 +1000

    tweak barrier shrinkage, remove logging

--- Scripts/Game/Entities/ScalerGuy.c
+++ Scripts/Game/Entities/ScalerGuy.c
@@ -4,7 +4,6 @@ class ScalerGuyClass: DAD_BaseTriggerEntityClass
 
 class ScalerGuy: DAD_BaseTriggerEntity
 {
-	// user script
 	AIWaypoint m_eWaypoint;
 
 	float m_fInitialTriggerRadius;
@@ -17,14 +16,10 @@ class ScalerGuy: DAD_BaseTriggerEntity
 
 	void UpdateBarrierEntity()
 	{
-		Print("Oh Hey!! UpdateBarrier is running!");
 		World w = GetGame().GetWorld();
 		IEntity barrier = w.FindEntityByName("Barrier");
-		if (!barrier)
-		{
-			Print("No Barrier found for Update!");
-			return;
-		}
+		if (!barrier) return;
+
 		if (m_fBarrierScale) barrier.SetScale(m_fBarrierScale);
 		if (m_vBarrierOrigin) barrier.SetOrigin(m_vBarrierOrigin);
 		barrier.Update();
@@ -32,42 +27,38 @@ class ScalerGuy: DAD_BaseTriggerEntity
 
 	void ScaleBarrier(float timeSlice)
 	{
-		Print("Scale Barrier");
 		RplComponent rplC = RplComponent.Cast(FindComponent(RplComponent));
-		//if (!Replication.IsServer()) return;
-		//if (!rplC || !rplC.IsOwner()) return;
+		if (!rplC || !rplC.IsOwner()) return;
 
-		if (Replication.IsClient()) return;
 		World w = GetGame().GetWorld();
 		IEntity barrier = w.FindEntityByName("Barrier");
 
-		if (true) {
-			DAD_FactionControlTriggerEntity trigger = DAD_FactionControlTriggerEntity.Cast(w.FindEntityByName("VictoryConditionUS"));
+		DAD_FactionControlTriggerEntity trigger = DAD_FactionControlTriggerEntity.Cast(w.FindEntityByName("VictoryConditionUS"));
 
-			float delta = (timeSlice * 3);
-			float radius = trigger.GetSphereRadius() - delta;
+		float delta = (timeSlice * 0.8);
+		float radius = trigger.GetSphereRadius() - delta;
 
-			trigger.SetSphereRadius(radius);
+		trigger.SetSphereRadius(radius);
+		if (m_eWaypoint)
+			m_eWaypoint.SetCompletionRadius(radius);
 
+		
+		//--- Calculate shrink transformation of Barrier
 
-			float scale = radius / m_fInitialTriggerRadius;
-			// Set the barrier size to a fraction of its initial size
-			m_fBarrierScale = scale * m_fInitialBarrierScale;
-			// slightly raise because greenhouse origin is below ground
-			m_vBarrierOrigin = barrier.GetOrigin() + ("0 0.15 0" * delta);
-			UpdateBarrierEntity();
-			Replication.BumpMe();
+		float scale = radius / m_fInitialTriggerRadius;
+		// Set the barrier size to a fraction of its initial size
+		m_fBarrierScale = scale * m_fInitialBarrierScale;
+		// slightly raise because greenhouse origin is below ground
+		m_vBarrierOrigin = barrier.GetOrigin() + ("0 0.15 0" * delta);
 
-			if (m_eWaypoint)
-				m_eWaypoint.SetCompletionRadius(radius);
-		}
+		UpdateBarrierEntity();
+		Replication.BumpMe();
 	}
 
 	void init()
 	{
 
 		RplComponent rplC = RplComponent.Cast(FindComponent(RplComponent));
-		Print("Game Start");
 
 		World w = GetGame().GetWorld();
 		IEntity barrier = w.FindEntityByName("Barrier");
@@ -87,25 +78,20 @@ class ScalerGuy: DAD_BaseTriggerEntity
 
 			Resource rplTest = Resource.Load("{E407D9BCB4B30DC6}Prefabs/MP/Test_RplEntity.et");
 		}
-		else
-		{
-		}
-		const float tick = 1 / 16;
+
+		const float tick = 1 / 32;
 		//const float tick = 2;
-		Print("End Game Start 1 ");
+
 		GetGame().GetCallqueue().CallLater(ScaleBarrier, tick * 1000, true, tick);
-		//GetGame().GetCallqueue().CallLater(barrier.Update, 500, true);
-		Print("End Game Start");
 	}
 
 	override void OnActivate(IEntity ent)
 	{
-		/* code here */
+		super.OnActivate(ent);
 	}
 
 	override void OnInit(IEntity owner)
 	{
-		/* code here */
 		super.OnInit(owner);
 		GetGame().GetCallqueue().CallLater(init, 200);
 	}
